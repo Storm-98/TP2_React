fc2af28330a83c143c2d240a7cb9c89ed6d2da79
Storm-98 <106162656+Storm-98@users.noreply.github.com>
2022-12-28 17:15:17 +0100

    Fetching data from url using axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState, useCallback} from 'react'
+import axios from 'axios'
+
+
+const apiURL = 'https://octave-api.sierrawireless.io/v5.0/orangesb/event/s636d2846c1fe893ca3acf014';
+
+const authAxios = axios.create({
+  baseURL: apiURL,
+  headers: {
+    "X-Auth-Token": "8ypwg55Yy4EK5lLPTshu6mx3mRMEBFTb",
+    "X-Auth-User": "sb_orange"
+  }
+}) 
 
 function App() {
+   const [data, setData] = useState([]);
+
+  useEffect(()=> {
+    authAxios.get()
+    .then(res =>{
+    console.log("Getting data from server", res.data.body)
+    setData(res.data.body)
+    }) 
+    .catch(err => console.log(err))
+
+  }, [])
+  
+  const array = data.map((data) => {
+    let date = new Date(data.creationDate) 
+    return (
+      <tr>
+      <td>{data?.id}</td>
+      <td>{data?.elems?.environment?.temperature}</td>
+      <td>{date?.toLocaleTimeString()}</td>
+      <td>{date?.toLocaleDateString()}</td>
+    </tr>
+
+    )
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      
+      {/* <button onClick={() => fetchData()}> Récupérer la température </button> */}
+      {/* {users.map(user => {
+        return <p key={user.id}>{user.name}</p>;
+      })} */}
+      <table>
+        <tr>
+          <th>id</th>
+          <th>Température</th>
+          <th>Heure de Prévelement</th>
+          <th>Date de création</th>
+        </tr>
+        {array}
+    </table>
+  </div>
   );
 }
 
